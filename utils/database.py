bbaebad6ec199f539ca9a34c6422241b26a36b24
Jake Goldman <jgoldman@stuy.edu>
2018-01-10 09:33:24 -0500

    merge issues

--- utils/database.py
+++ utils/database.py
@@ -11,10 +11,10 @@ def create_db():
     c.execute("CREATE TABLE users (user TEXT PRIMARY KEY, pass TEXT, favorites TEXT);")
     
     #creates teams table
-    c.execute("CREATE TABLE teams (teamid TEXT PRIMARY KEY, user TEXT, name TEXT, version TEXT, weaknesses TEXT, strengths TEXT, upvotes INT);")
+    c.execute("CREATE TABLE teams (teamid INT PRIMARY KEY, user TEXT, name TEXT, version TEXT, weaknesses TEXT, strengths TEXT, upvotes INT);")
     
     #creates pokemon table
-    c.execute("CREATE TABLE pokemon (pkmnid TEXT PRIMARY KEY, teamid TEXT, species TEXT, gender TEXT, level INT, ability TEXT, moves TEXT, item TEXT, nature TEXT);")
+    c.execute("CREATE TABLE pokemon (pkmnid INT PRIMARY KEY, teamid INT, species TEXT, gender TEXT, level INT, ability TEXT, moves TEXT, item TEXT, nature TEXT);")
 
     db.commit()
     db.close()
@@ -46,17 +46,81 @@ def add_favorite(username, teamid);
     fav_s = fav_s + "," + teamid
 
     #updating database
-    c.execute("UPDATE users SET favorites = \"%s\" WHERE user = \"%s\";;" %(fav_s, username))
+    c.execute("UPDATE users SET favorites = \"%s\" WHERE user = \"%s\";" %(fav_s, username))
 
     db.commit()
     db.close()
 
+#finds username
+def find_user(username)
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    #looks through users table
+    c.execute("SELECT user FROM users WHERE user = \"%s\";" %(username))
+    found = c.fetchone()[0]
+
+    #returns true if username is found
+    if found == username:
+        return True
+    else:
+        return False
+
+    db.commit()
+    db.close()
+
+#find password
+def match_pass(username, password)
+    db = sqlite3.connect(f)
+    c = db.cursor()
+    
+    #looks through table for pass
+    c.execute("SELECT pass FROM users WHERE user = \"%s\";" %(username))
+    found = c.fetchone()[0]
+
+    #returns if password is found
+    if found == password:
+        return True
+    else:
+        return False
+    
+    db.commit()
+    db.close()
+    
 #creates a new team
 def new_team(username, name, version, weaknesses, strengths, pkmnlist)
     db = sqlite3.connect(f)
     c = db.cursor()
 
-    #not done
+    #creating a new teamid
+    c.execute("SELECT teamid FROM teams ORDER BY teamid DESC LIMIT 1;")
+    teamid = c.fetchone()[0]
+    teamid += 1
+
+    #adding team to table
+    c.execute("INSERT INTO teams VALUES(%d, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", 0);" %(teamid, username, name, version, weaknesses, strengths))
     
     db.commit()
     db.close()
+
+
+#creating a new pokemon
+def create_poke(teamid, species, gender, level, ability, moves, item, nature)
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+    #creating a new teamid
+    c.execute("SELECT teamid FROM teams ORDER BY teamid DESC LIMIT 1;")
+    pkmnid = c.fetchone()
+    pkmnid += 1
+
+    #adding team to table
+    c.execute("INSERT INTO pokemon VALUES(%d, %d, \"%s\", \"%s\", %d, \"%s\", \"%s\", \"%s\", \"%s\");" %(pkmnid, teamid, species, gender, level, ability, moves, item, nature))
+    
+    db.commit()
+    db.close()
+
+    
+#updating pokemon
+def update_poke(
+
