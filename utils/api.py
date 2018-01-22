b13e1a645e28ecea4469e57728195cefaee4b233
Jake Goldman <jgoldman@stuy.edu>
2018-01-22 01:27:29 -0500

    began working on strengths/weaknesses

--- utils/api.py
+++ utils/api.py
@@ -16,5 +16,37 @@ def search_poke(pokemon):
     dic = search_api("pokemon/" + pokemon)
     return dic
 
+
+
+# ============ STRENGTHS/WEAKNESSES ===============
+
+# Returns a (formated) dictionary with the type effectiveness for the given type
+def type_info(type):
+    dmg_rel = search_api("type/" + type)["damage_relations"]
+    for relation in dmg_rel:
+        type_list = []
+        for type_entry in dmg_rel[relation]:
+            type_list.append( dmg_rel[relation][type_entry]["name"] )
+        dmg_rel[relation] = type_tist
+    return dmg_rel
+
+
+
+# given a list of pokemon, returns a dictionary keyed by type with values equal
+# to the strength of the team against that type
+def get_strengths(team):
+    return None
+
+# given a list of pokemon, returns a dictionary keyed by type with values equal
+# to the weakness of the team against that type
+def get_weaknesses(team):
+    return None
+
+# given a list of pokemon, returns a dictionary keyed by type with values equal
+# to the resistance of the team against that type
+def get_resistance(team):
+    return None
+
 if __name__ == '__main__':
-    print search_poke("pikachu")
+    # print search_poke("pikachu")
+    # print search_api("type/ground")
